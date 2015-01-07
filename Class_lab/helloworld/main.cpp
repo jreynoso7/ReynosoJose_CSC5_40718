4f5aaed38325feab4ea38083ba235a02d20b52c9
jreynoso7 <jreynoso7@student.rcc.edu>
2015-01-06 20:03:07 -0800

    Signed-off-by: jreynoso7 <jreynoso7@student.rcc.edu>

---
+++ Class_lab/helloworld/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose R.
+ * Created on January 6, 2015, 6:05 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//global constants
+
+//function prototypes
+
+//execution begins here
+int main(int argc, char** argv) {
+    //output hello world
+    cout<<"hello world"<<endl;
+    //fin
+    return 0;
+}
+
